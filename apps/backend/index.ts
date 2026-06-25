62efc98ecfe541b4dd4efeaa45e20129612c9ddd
Pushparaj <psinghparmar42@gmail.com>
2026-06-25 11:15:49 +0530

    first commit

---
+++ apps/backend/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+app.post("/api/v1/signup", (req, res) => {});
+app.post("/api/v1/signin", (req, res) => {});
+app.post("/api/v1/avatar", (req, res) => {});
+app.post("/api/v1/video", (req, res) => {});
+app.get("/api/v1/video/:videoId", (req, res) => {});
+app.get("/api/v1/videos", (req, res) => {});
+app.get("/api/v1/models", (req, res) => {});
+app.get("/api/v1/avatar/:avatarId", (req, res) => {});
+app.get("/api/v1/avatars", (req, res) => {});
